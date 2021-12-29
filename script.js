049547d9b4df41ab1feb76e7457d0bbac8fc39cc
Andersonclayton987 <andersonclayton987@gmail.com>
2021-12-29 16:16:58 -0300

    Atualização

--- script.js
+++ script.js
@@ -42,13 +42,12 @@ $(document).ready(function(){
     $('.carousel').owlCarousel({
         margin:20,
         loop: true,
-        autoplayTimeOut: 2000,
+        autoplayTimeOut: 2500,
         autoplayHoverPause: true,
         responsive:{
             0:{
                 items:1,
                 nav:false
-
             },
         }
     })
