f03f374ba1899e8517817ad832ee82a49ec99a7a
Andersonclayton987 <andersonclayton987@gmail.com>
2021-12-29 16:13:59 -0300

    Atualização

--- script.js
+++ script.js
@@ -48,13 +48,8 @@ $(document).ready(function(){
             0:{
                 items:1,
                 nav:false
-<<<<<<< HEAD
+
             },
-            
-            
-=======
-            },                     
->>>>>>> 3052357fbdfe55055332077f32f8bb1fdb6cbea0
         }
     })
 })
